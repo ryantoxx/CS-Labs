e64e263756a368cfae1cea5ae562400cb2e112e5
ryantoxx <114425150+ryantoxx@users.noreply.github.com>
2023-11-03 21:12:54 +0200

    update: small fix

--- Lab 4 - DES Cipher/main.py
+++ Lab 4 - DES Cipher/main.py
@@ -51,15 +51,20 @@ class DesCipher:
 
     @staticmethod
     def calculate_Ri(Li_minus_1, Ri_minus_1, Round_Key):
-        expanded_Ri = DesCipher.get_expansion(Ri_minus_1)
-        print(f"Expanded Ri from 32 bits to 48 bits: {expanded_Ri}.")
-        xor_operation = ''.join(str(int(a) ^ int(b)) for a, b in zip(Round_Key, expanded_Ri))
-        print(f"The XOR operation between Round Key and the expanded Ri(48 bits): {xor_operation}.")
+        
+        expanded_Ri_minus_1 = DesCipher.get_expansion(Ri_minus_1)
+        print(f"Expansion Permutation of Ri-1: {expanded_Ri_minus_1}. Length = {len(expanded_Ri_minus_1)} bits.")
+        
+        xor_operation = ''.join(str(int(a) ^ int(b)) for a, b in zip(Round_Key, expanded_Ri_minus_1))
+        print(f"The XOR operation between Round Key and the expanded Ri-1: {xor_operation}. Length = {len(xor_operation)} bits.")
+        
         S_box_result = DesCipher.get_box_substitution(xor_operation)
-        print(f"The bits were ran through S-Boxes(32 bits): {S_box_result}.")
+        print(f"The bits were ran through S-Boxes: {S_box_result}. Length = {len(S_box_result)} bits.")
+        
         P_box_mix = DesCipher.get_Pbox_permutation(S_box_result)
-        print(f"Permutation of the 32-bit output: {P_box_mix}.")
+        print(f"P-Box Permutation of the 32-bit output: {P_box_mix}. Length = {len(P_box_mix)} bits.")
         Ri = ''.join(str(int(Li_minus_1[i]) ^ int(P_box_mix[i])) for i in range(32))
+        
         return Ri
 
     @staticmethod
@@ -68,27 +73,28 @@ class DesCipher:
         for i in range(0, len(P_box), 8):
             print(" ".join(f"{num:2d}" for num in P_box[i:i+8]))
 
-    # @staticmethod
+    @staticmethod
     def print_E_box():
         print("E-box:")
         for i in range(0, len(E_box), 6):
             print(" ".join(f"{num:2d}" for num in E_box[i:i+6]))
 
 if __name__ == "__main__":
-    DesCipher.print_P_box()
-    print("\n")
     DesCipher.print_E_box()
     print("\n")
+    DesCipher.print_P_box()
+    print("\n")
+    
 
     Li_minus_1 = DesCipher.generate_Li_minus_1()
     Ri_minus_1 = DesCipher.generate_Ri_minus_1()
     Round_Key = DesCipher.generate_Round_Key()
-    print(f"Li-1: {Li_minus_1}.")
-    print(f"Ri-1: {Ri_minus_1}.")
-    print(f"Round Key: {Round_Key}.")
+    print(f"Li-1: {Li_minus_1}. Length = {len(Li_minus_1)} bits.")
+    print(f"Ri-1: {Ri_minus_1}. Length = {len(Ri_minus_1)} bits.")
+    print(f"Round Key: {Round_Key}. Length = {len(Round_Key)} bits.")
 
     Ri = DesCipher.calculate_Ri(Li_minus_1, Ri_minus_1, Round_Key)
-    print("Ri for the given Li-1 is:", Ri)
+    print(f"Ri after the XOR operation with the given Li-1 is: {Ri}. Length = {len(Ri)} bits.\n")
 
 
 
